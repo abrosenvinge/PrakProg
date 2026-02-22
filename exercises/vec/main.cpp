02c385639ff7c06e96ec9161011a489bebae721a
Asbjørn Rosenvinge <abrosenvinge@gmail.com>
2026-02-22 11:13:47 +0100

    templated vec

--- exercises/vec/main.cpp
+++ exercises/vec/main.cpp
@@ -1,10 +1,11 @@
 #include "vec.hpp"
 #include <format>
 #include <iostream>
+#include <complex>
 
 int main() {
-	vec::vec v(1,2,3);
-	vec::vec u(4,5,6);
+	vec::vec<double> v(1,2,3);
+	vec::vec<double> u(4,5,6);
 
 	std::cout << "v = " << v << "\n";
 	std::cout << "-v = " << -v << "\n";
@@ -18,9 +19,9 @@ int main() {
 
 	std::cout << "u + v = " << u + v << "\n";
 
-	std::cout << "5*v = " << 5*v << "\n";
-	std::cout << "v*5 = " << v*5 << "\n";
-	std::cout << "v/5 = " << v/5 << "\n";
+	std::cout << "5*v = " << 5.*v << "\n";
+	std::cout << "v*5 = " << v*5. << "\n";
+	std::cout << "v/5 = " << v/5. << "\n";
 
 	std::cout << "v.norm() = " << v.norm() << "\n";
 	std::cout << "dot(u,v) = " << vec::dot(u,v) << "\n";
@@ -30,5 +31,12 @@ int main() {
 
 	std::cout << std::format("u == v: {}\n", vec::approx(u,v)); //should be false
 	std::cout << std::format("approx(u,u + (1e-9, 1e-9, 1e-9)): {}", 
-			vec::approx(u, u + vec::vec(1e-9, 1e-9, 1e-9)));
+			vec::approx(u, u + vec::vec(1e-9, 1e-9, 1e-9))) << "\n";
+
+	const std::complex<double> Im = std::complex(0.,1.);
+	const std::complex<double> Re = std::complex(1.,0.);
+	vec::vec<std::complex<double>> complex_v(Re, Im, Re + Im);
+	std::cout << complex_v << "\n";
+	complex_v *= 5;
+	std::cout << complex_v << "\n";
 }
--- exercises/vec/vec.hpp
+++ exercises/vec/vec.hpp
@@ -1,14 +1,21 @@
 #pragma once
 
 #include <ostream>
+#include <cmath>
+#include "../epsilon/approx.hpp"
 
 namespace vec {
+	template <typename T>
 	struct vec {
-		double x,y,z;
+		T x,y,z;
 		double norm();
 
 		// constructors
-		vec(double x, double y, double z) : x(x), y(y), z(z) {} // parametrized
+		vec(T x, T y, T z) {
+			this->x = x;
+			this->y = y;
+			this->z = z;
+		} // parametrized
 		vec() : vec(0,0,0) {} // default
 		vec(const vec&) = default; // copy: a(b)
 		vec(vec&&) = default; // move: a(b+c)
@@ -20,23 +27,90 @@ namespace vec {
 		vec& operator =(const vec&) = default; //copy: a = b
 		vec& operator =(vec&&) = default; //move: a = b + c
 
-		vec& operator+=(const vec&);
-		vec& operator-=(const vec&);
-		vec& operator/=(double);
-		vec& operator*=(double);
+		vec& operator+=(const vec& other) {
+			x += other.x;
+			y += other.y;
+			z += other.z;
+			return *this;
+		}
+		vec& operator-=(const vec& other) {
+			x -= other.x;
+			y -= other.y;
+			z -= other.z;
+			return *this;
+		}
+		vec& operator/=(T a) {
+			x /= a;
+			y /= a;
+			z /= a;
+			return *this;
+		}
+		vec& operator*=(T a) {
+			x *= a;
+			y *= a;
+			z *= a;
+			return *this;
+		}
 
-		friend std::ostream& operator<<(std::ostream&, const vec&);
+		friend std::ostream& operator<<(std::ostream& s, const vec& u) {
+			s << u.x << " " << u.y << " " << u.z;
+			return s;
+		}
 	};
 
-	vec operator -(vec);
-	vec operator +(vec, const vec&);
-	vec operator -(vec, const vec&);
-	vec operator *(vec, double);
-	vec operator *(double, vec);
-	vec operator /(vec, double);
+	template<typename T>
+	vec<T> operator -(vec<T> u) {
+		u *= -1;
+		return u;
+	}
 
-	double dot(const vec&, const vec&); // dot product
-	vec cross(const vec&, const vec&);
+	template<typename T>
+	vec<T> operator+(vec<T> u, const vec<T>& v) {
+		u += v;
+		return u;
+	}
 
-	bool approx(const vec&, const vec&);
+	template<typename T>
+	vec<T> operator*(T a, vec<T> u) {
+		u *= a;
+		return u;
+	}
+
+	template<typename T>
+	vec<T> operator*(vec<T> u, T a) {
+		u *= a;
+		return u;
+	}
+
+	template<typename T>
+	vec<T> operator/(vec<T> u, T a) {
+		u /= a;
+		return u;
+	}
+
+	template<typename T>
+	double dot(const vec<T>& u, const vec<T>& v) {
+		return u.x * v.x + u.y * v.y + u.z * v.z;
+	}
+
+	template<typename T>
+	double vec<T>::norm() {
+		return std::sqrt(dot(*this, *this));
+	}
+
+	template<typename T>
+	vec<T> cross(const vec<T>& u, const vec<T>& v) {
+		return vec(
+			u.y*v.z - u.z*v.y,
+			u.z*v.x - u.x*v.z,
+			u.x*v.y - u.y*v.x
+		);
+	}
+
+	template<typename T>
+	bool approx(const vec<T>& u, const vec<T>& v) {
+		return approx::approx(u.x,v.x)
+				&& approx::approx(u.y,v.y)
+				&& approx::approx(u.z,v.z);
+	}
 }
