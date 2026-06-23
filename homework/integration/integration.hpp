156bd9f5233552b47c878d4ac1950ef2a9fdf46e
Asbjørn Rosenvinge <abrosenvinge@gmail.com>
2026-06-23 12:50:58 +0200

    make normal and CC use same acc and eps

--- homework/integration/integration.hpp
+++ homework/integration/integration.hpp
@@ -3,13 +3,29 @@
 #include <tuple>
 
 namespace pp {
-	std::tuple<double,double> integrate(const std::function<double(double)>& f, double a, double b, double acc = 0.000001, double eps = 0.000001);
+	std::tuple<double,double> integrate(const std::function<double(double)>& f, 
+			double a,
+			double b,
+			double acc = 0.0001,
+			double eps = 0.0001);
 
-	std::tuple<double,double> rai(const std::function<double(double)>& f, double a, double b, double acc, double eps, double f2, double f3);
+	std::tuple<double,double> rai(const std::function<double(double)>& f,
+			double a,
+			double b,
+			double acc,
+			double eps,
+			double f2,
+			double f3);
 
-	std::tuple<double,double> Clenshaw_Curtis_quad(const std::function<double(double)>& f, double a, double b, double acc = 0.0001, double eps = 0.0001);
+	std::tuple<double,double> Clenshaw_Curtis_quad(const std::function<double(double)>& f, 
+			double a, 
+			double b, 
+			double acc = 0.0001, 
+			double eps = 0.0001);
 
-	std::tuple<std::function<double(double)>,double,double> transform_inf_interval(const std::function<double(double)>& f, double a, double b);
+	std::tuple<std::function<double(double)>,double,double> transform_inf_interval(const std::function<double(double)>& f,
+			double a,
+			double b);
 
 	double erf(double z, double acc = 0.001, double eps = 0.001);
 } // namespace pp
